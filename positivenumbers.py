fc1ada31785b8f450c5333c697e0b99639b87c24
Simmi070 <Lalwanisimmi1212@gmail.com>
2026-09-24 17:51:23 +0200

    positive and negative

--- positivenumbers.py
+++ positivenumbers.py
@@ -3,6 +3,7 @@ locations = [(1, 2), (4, 0), (-2, 5), (3, 3)]
 print(locations)
 print()
 number = 0
+numberneg = 0
 for one, two in locations:
     if one >= 0:
         print(one, "is positive")
@@ -14,8 +15,10 @@ for one, two in locations:
             print()
     else:
         print(one, "isn't positive")
+        numberneg = numberneg + 1
         print("Let's check the next one!")
         print()
 
 
 print("The total amount of positive numbers are: ", number)
+print("And the total amount of negative numbers are: ", numberneg)
